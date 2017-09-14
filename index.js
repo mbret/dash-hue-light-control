1274ea8a66ec49ba9f5c4060e4faaee0de9ce381
Maxime Bret <xmax54@gmail.com>
2017-09-14 19:05:13 +0200

    Add cli and better error checking

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv
+
+// we need to deactivate console as node-dash-button output data by itself -_-
+// we will use another way to log data
+console.error = () => {}
+
+const program = require('commander')
 const watcher = require('./lib/watcher')
 const configLoader = require('./lib/config-loader')
+const cli = require('./lib/cli')
+let winston = require('winston');
 
-// args check
-if (!argv.settings) {
-  console.log('Argument --settings is missing. Please provide --settings path to use this program. Exiting app...')
-  process.exit()
+program
+  .version(require('./package.json').version)
+  .usage(cli.getUsage())
+
+program
+  .command('help')
+  .description('output usage information')
+  .action(() => {
+    program.outputHelp()
+  })
+
+program
+  .command('start')
+  .description('Watch for your dash button')
+  .option('--settings <path>', 'settings path')
+  .action((opts) => {
+    // check arg
+    if (!opts.settings) {
+      winston.info('option --settings is is required.')
+      process.exit()
+    }
+
+    let settings
+
+    // load and check settings
+    try {
+      settings = configLoader.load(opts.settings)
+    } catch (err) {
+      if (err.code === 'MODULE_NOT_FOUND') {
+        winston.info(`Unable to retrieve settings from path [${opts.settings}]. Please check that [${opts.settings}] is a correct path`)
+      } else{
+        winston.info(`Unable to load your settings at [${opts.settings}]. Please verify that your file is in valid json format`)
+      }
+      process.exit()
+    }
+
+    // run watcher
+    try {
+      watcher(settings)
+    } catch (err) {
+      winston.warn(`An unexpected error occurred. The program will stop.`, err)
+    }
+  })
+
+program
+  .command('*')
+  .action(() => {
+    winston.info('Command not found');
+    program.outputHelp();
+    process.exit();
+  });
+
+// must be before .parse() since
+// node's emit() is immediate
+
+if (process.argv.length === 2) {
+  program.parse(process.argv);
+  program.outputHelp();
+  // Check if it does not forget to close fds from RPC
+  process.exit();
 }
 
-// run watcher
-watcher(configLoader.load(argv.settings))
+program.parse(process.argv)
+
